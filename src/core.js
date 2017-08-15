34bd0777497367fd62edf57b4951917b654cd658
Mark Mahoney <Mark@Marks-MacBook-Pro.local>
2017-08-14 22:47:58 -0400

    Decide on basic data structure and create pure functions to create new polls

--- src/core.js
+++ src/core.js
@@ -1,19 +1,114 @@
-import {List} from 'immutable';
-import {Map} from 'immutable';
+import {List, Map, fromJS} from 'immutable';
 
+// test function
 export function setEntries(state,entries){
   return state.set('entries',List(entries));
 }
 
-// adds one vote to choice
+/* code snippets for later
+if (!choices || choices.length < 3){
+  throw "Instant runoff vote requires at least 3 choices to work"
+}
+
+///////////////
+
+for (var i = 0; i<choices.length;i++){
+  choices[i] = {"choice":choices[i],tally:{}};
+}
+choices = fromJS(choices);
+///////////////////
+
+
+return state.updateIn(["polls"],polls => polls.push(Map({
+  id:state.get('polls').size,
+  q:q,
+  choices:choices,
+  votes:List()
+})));
+*/
+
+
+
+
+// CREATING AND EDITING POLLS
+// create a new poll
+export function createPoll(state){
+  return state.updateIn(['polls'],polls => polls.push(Map({
+    pollId:state.get('polls').size,
+    choices:List()
+  })));
+}
+
+// edit the poll question
+export function editQuestion(state,pollId,q){
+  return state.setIn(['polls',pollId,"q"],q);
+}
+
+// add a choice to the poll
+export function addChoice(state,pollId,choice){
+
+  return state.updateIn(['polls',pollId,"choices"],
+    choices => choices.push(Map({
+        choiceId:state.get('polls').get(pollId).get('choices').size,
+        choice:choice
+      }))
+    
+  )
+}
+
+// edit an old poll -- THIS SHOULD BE BROKEN DOWN
+// edit question
+// edit choice
+
+// break this down to smaller pieces
+export function editPoll(state,id,q,choices){
+  if (!choices || choices.length < 3){
+    throw "Instant runoff vote requires at least 3 choices to work"
+  }
+
+  for (var i = 0; i<choices.length;i++){
+    choices[i] = fromJS(choices[i]).update({"choice":choices[i]});
+  }
+  choices = fromJS(choices);
+
+  return state.setIn(["polls",id],Map({
+    id:id,
+    q:q,
+    choices:choices,
+    votes:List()
+  }));
+}
+/*to do
+rename id to pollId
+*/
+
+// edit a poll
+
+// delete a poll
+
+// add a choice
+
+// edit a choice
+
+// remove a choice
+
+// VOTING
+
+// cast a vote
+export function castVote(state,id,voter,vote){
+  return state.updateIn(["polls",id,"votes"],votes => votes.push(Map({
+    voter:voter,
+    vote:vote
+  })))
+}
+
+
+// COUNTING
+
+// find the winner and runner up
+
+
+// add one vote to choice
 export function addVote(state, poll, choice){
   return state.setIn(["polls",poll,choice],state.getIn(["polls",poll,choice])+1);
 }
-
-/*
-state
-- polls
--- [
--- {name:"", if:"" choices:{choice:"#"} }
--- ]
-*/
--- test/core_spec.js
+++ test/core_spec.js
@@ -1,8 +1,235 @@
 import {List, Map, fromJS} from 'immutable';
 import {expect} from 'chai';
 
-import {setEntries, addVote} from '../src/core';
+import {setEntries, addVote, createPoll, editPoll, castVote, editQuestion, addChoice} from '../src/core';
 
+var establishedState = fromJS({
+  polls: [
+    {
+      id:0,
+      q:"What's the best color?",
+      choices:[
+        {
+          choice:"blue",
+          votes:[
+            {
+              voter:'markrmahoney',
+              alternatives:["red","yellow"]
+            },
+            {
+              voter:'mrsmith123',
+              alternatives:["blue"]
+            }
+          ],
+          tally:{
+            self:5,
+            red:2,
+            yellow:1
+          }},
+        "red",
+        "yellow"],
+      votes:[
+        {
+          voter:'markrmahoney',
+          preference:["red","yellow"]
+        },
+        {
+          voter:'mrsmith123',
+          preference:["blue"]
+        }
+      ]
+    },
+    {
+      id:1,
+      q:"What's the best number?",
+      choices:["1","13","pi"],
+      votes:[
+        {
+          voter:'markrmahoney',
+          preference:["1","pi","13"]
+        },
+        {
+          voter:'mrsmith123',
+          preference:["pi"]
+        }
+      ]
+
+    }
+  ],
+})
+
+describe('POLLS:', ()=> {
+
+  describe('createPoll()', () => {
+    it ('Adds a new poll to the state', () => {
+      const state = fromJS({
+        polls:[
+          {pollId:0},
+          {pollId:1}
+        ]
+      });
+      const nextState = createPoll(state);
+      expect(nextState).to.equal(fromJS({
+        polls: [
+          {pollId:0},
+          {pollId:1},
+          {pollId:2,choices:List()}
+        ]
+      }));
+    })
+  })
+
+  describe('editQuestion()', () => {
+    const state = fromJS({
+      polls:[
+        {pollId:0},
+        {pollId:1,q:"Best city?"}
+      ]
+    });
+    it ('Updates existing question', () => {
+      expect(editQuestion(state,1,"Favorite color?")).to.equal(fromJS({
+        polls: [
+          {pollId:0},
+          {pollId:1,q:"Favorite color?"}
+        ]
+      }));
+    })
+    it ("Adds a new question if one doesn't already exist", () => {
+      expect(editQuestion(state,0,"Favorite color?")).to.equal(fromJS({
+        polls: [
+          {pollId:0, q:"Favorite color?"},
+          {pollId:1,q:"Best city?"}
+        ]
+      }));
+    })
+  })
+
+  describe('addChoice()', () => {
+    const state = fromJS({
+      polls:[
+        {pollId:0,choices:List()},
+        {
+          pollId:1,
+          q:"Best city?",
+          choices:[
+            {choiceId: 0, choice:"NY"},
+            {chocieId: 1, choice:"LA"}
+          ]
+        }
+      ]
+    })
+    it('Adds a choice when none exist', () => {
+      expect(addChoice(state,0,"New choice!")).to.equal(fromJS({
+        polls:[
+          {
+            pollId:0,
+            choices:[
+              {choiceId:0,choice:"New choice!"}
+            ]
+          },
+          {
+            pollId:1,
+            q:"Best city?",
+            choices:[
+              {choiceId: 0, choice:"NY"},
+              {chocieId: 1, choice:"LA"}
+            ]
+          }
+        ]
+
+      }))
+    })
+    it('Adds a choice to an existing list of choices', () => {
+
+    })
+  })
+/*
+  describe('editPoll()', () => {
+    it ('Edits the question and choices for the poll', () => {
+      const state = fromJS({
+        polls: [
+          {id:0},
+          {id:1},
+          {
+            id:2,
+            q:"Who should be president?",
+            choices:[
+              {choice:"Mario",tally:{}},
+              {choice:"Luigi",tally:{}},
+              {choice:"Bowser",tally:{}}
+            ],
+            votes:List()
+          }
+        ]
+      });
+      const q = "Who should be vice president?";
+      const choices = ['Mario','Luigi','Bowser'];
+      const id = 2;
+      const nextState = editPoll(state,id,q,choices);
+      expect(nextState).to.equal(fromJS({
+        polls: [
+          {id:0},
+          {id:1},
+          {
+            id:id,
+            q:q,
+            choices:["Mario","Luigi","Bowse"],
+            votes:List()
+          }
+        ]
+      }));
+    })
+  })*/
+})
+
+
+describe("VOTING",() => {
+  describe("castVote()", () => {
+    it ("adds a new vote object to array of votes in poll",()=>{
+      const state = fromJS({
+        polls: [
+          {id:0},
+          {id:1},
+          {
+            id:2,
+            q:"Who should be president?",
+            choices:["Mario","Luigi","Peach"],
+            votes:List()
+          }
+        ]
+      })
+      const vote = List(["Mario","Peach"]);
+      const id = 2;
+      const voter = 'mrkmhny';
+      const nextState = castVote(state, id, voter, vote);
+      expect(nextState).to.equal(fromJS({
+        polls: [
+          {id:0},
+          {id:1},
+          {
+            id:2,
+            q:"Who should be president?",
+            choices:["Mario","Luigi","Peach"],
+            votes:[{
+              voter:voter,
+              vote:vote
+            }]
+          }
+        ]
+      }))
+    })
+  })
+})
+
+describe("TALLYING",()=>{
+  describe("tallyResults()",()=>{
+
+  })
+})
+
+
+
+/*
 describe('application logic', ()=> {
   describe('setEntries', () => {
     it('adds the entries to the state', () => {
@@ -46,4 +273,4 @@ const state = fromJS({
     'poll456':{'question':"home town",'decision1':3,'decision2':5},
   }
 });
-console.log(addVote(state,"poll123","choice1"));
+*/
