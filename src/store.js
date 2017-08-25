884af64073ef4e675a32582cb8f269ebb3ecd3c2
Mark Mahoney <Mark@Marks-MacBook-Pro.local>
2017-08-25 19:42:58 -0400

    Adding reducers and store

--- src/store.js
+++ src/store.js
@@ -1,6 +1,6 @@
 import {createStore} from 'redux';
 
-import {reducer} from './src/reducer'
+import reducer from './reducer.js';
 
 export default function makeStore(){
   return createStore(reducer);
--- test/store_spec.js
+++
@@ -1,4 +0,0 @@
-import {Map, fromJS} from 'immutable';
-import {expect} from 'chai';
-
-import makeStore from '../src/store';
